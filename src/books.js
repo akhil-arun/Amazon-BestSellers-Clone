f58ad3a41d511dc93e9b5dec6e6b5345282f9fe6
akhiller30 <akhil25adhu@gmail.com>
2023-08-10 21:38:24 -0700

    first commit

---
+++ src/books.js
@@ -0,0 +1,20 @@
+export const books = [
+    {
+        author: " Rebecca Yarros ",
+        title: 'Fourth Wing (The Empyrean, 1)',
+        img: "https://images-na.ssl-images-amazon.com/images/I/91n7p-j5aqL._AC_UL900_SR900,600_.jpg",
+        id: 1
+    },
+    {
+        author: "Alex Snodgrass",
+        title: "Dinner Tonight: 100 Simple, Healthy Recipes",
+        img: "https://images-na.ssl-images-amazon.com/images/I/811HITbWChL._AC_UL900_SR900,600_.jpg",
+        id: 2
+    },
+    {
+        author: "James Clear",
+        title: "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
+        img: "https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL900_SR900,600_.jpg",
+        id: 3
+    }
+];
